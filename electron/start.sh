3b6bed7b90edfbea9f7409404ae1ab208dac8c26
pranavjoy <pranavjoy@protonmail.com>
2020-03-03 12:32:26 +0100

    Update start.sh

--- electron/start.sh
+++ electron/start.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
 # Echo the localhost value
 echo "Setting hostname..."
 echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
-
 # By default docker gives us 64MB of shared memory size but we need more for visuals
 umount /dev/shm && mount -t tmpfs shm /dev/shm
 echo "Increased memory allocation"
-
 # Remove any temp data
 rm /tmp/.X0-lock &>/dev/null || true
 echo "Removed temporary startx data"
-
 # Check to see if the noVNC_PASSWORD has been set
 if [[ -z "${NOVNC_PASSWORD}" ]]; then
       while :; do
@@ -23,14 +19,11 @@ if [[ -z "${NOVNC_PASSWORD}" ]]; then
 else
       echo "\$NOVNC_PASSWORD is set"
 fi
-
 # Set the X11VNC Password
 echo "Storing X11 VNC password..."
 mkdir ~/.x11vnc
 x11vnc -quiet -storepasswd ${NOVNC_PASSWORD} ~/.x11vnc/passwd
-
 echo "Setup complete"
-
 # Start the UI pointing to ElectronJS
 startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --no-sandbox -- -nocursor &
 P1=$!
